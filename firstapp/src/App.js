c6f1837ab270f34722cf1f982a0f98399783cfcb
SreeBharathiR <bharathisree130@gmail.com>
2024-10-28 14:57:48 +0530

    Day 6

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import FirstComponent from "./components/FirstComponent";
@@ -6,10 +6,70 @@ import StateComponent from "./components/StateComponent";
 import Button from "./components/Button";
 import CounterContext, { CounterProvider } from "./context/CounterContext";
 
+const initialState = {
+  user: { name: "Sree", id: 1 },
+  tasks: [{ name: "task 1", id: 1 }],
+};
+const reducerFunc = (curentState, action) => {
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...curentState,
+        tasks: [
+          ...curentState.tasks,
+          { name: action.payload.name, id: action.payload.id },
+        ],
+      };
+    case "DELETE":
+      return {};
+    default:
+      return curentState;
+  }
+};
 function App() {
+  const [typedValue, setTypedValue] = useState("");
+  const [taskState, taskDispatch] = useReducer(reducerFunc, initialState);
+  function handleAdd() {
+    taskDispatch({
+      type: "ADD",
+      payload: { name: typedValue, id: Date.now() },
+    });
+    setTypedValue("");
+  }
+  function handleDelete(id) {
+    taskDispatch({
+      type: "DELETE",
+      payload: id,
+    });
+  }
   return (
     <>
-      <CounterProvider>
+      <h1>Task Manager</h1>
+      <p>{taskState.user.name}</p>
+      <input
+        type="text"
+        value={typedValue}
+        onChange={(e) => {
+          setTypedValue(e.target.value);
+        }}
+      ></input>
+      <button onClick={handleAdd}>ADD</button>
+      <h3>Tasks:</h3>
+      {taskState.tasks.map((val) => (
+        <>
+          {" "}
+          <li key={val.id}>{val.name}</li>
+          <button
+            onClick={() => {
+              handleDelete(val.id);
+            }}
+          >
+            Delete
+          </button>
+        </>
+      ))}
+      {console.log(typedValue)}
+      {/* <CounterProvider>
         <Counter></Counter>
       </CounterProvider>
       {/* <Counter data={count}>
@@ -19,7 +79,8 @@ function App() {
           func={decrement}
           disable={count < 1 ? true : false}
         />
-      </Counter> */}
+      </Counter> */}{" "}
+      {/* */}
     </>
   );
 }
